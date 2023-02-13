bc100ff6c43edc66aa0bfb284fbf3af22cc3a6c1
Andresa Alves <andresa_15ga@hotmail.com>
2023-02-13 20:21:20 -0300

    começando a implementar o javascript

--- calculo.js
+++ calculo.js
@@ -1,77 +1,108 @@
-function calculadora() {
-    const operacao = Number(prompt("Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão Real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)"));
+const calculator = document.querySelector('#calculator')
+const keys = calculator.querySelector('#keyboard')
+const displayPress = document.querySelector('#last-calc')
+const display = document.querySelector('#resul-screen')
 
-    if(!operacao || operacao >= 9) {
-        alert('Erro - Operação inválida!');
-        calculadora()
-    } else {    
-        let n1 = Number(prompt("Insira o primeiro valor: "));
-        let n2 = Number(prompt("Insira o segundo valor: "));
-        let resultado;
 
-        function soma() {
-            resultado = n1 + n2;
-            alert(`${n1} + ${n2} = ${resultado}`)
-            novaOperacao()
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = displayPress.textContent
+
+        const calculate = (n1, operator, n2) => {
+            let result = ''
+
+            if (operator === '+') {
+                result = parseFloat(n1) + parseFloat(n2)
+            } else if (operator === '-') {
+                result = parseFloat(n1) - parseFloat(n2)
+            } else if (operator === 'x') {
+                result = parseFloat(n1) * parseFloat(n2)
+            } else if (operator === '/') {
+                result = parseFloat(n1) / parseFloat(n2)
+            } else if (operator === '%') {
+                result = parseFloat(n1) * (parseFloat(n2) / 100)
+            }
+
+            return result
+        }
+
+        if (!action) {
+            if (
+                displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate'
+            ) {
+                displayPress.textContent = keyContent
+            } else {
+                displayPress.textContent = displayedNum + keyContent
+            }
+            calculator.dataset.previousKeyType = 'number'
+        }
+
+        if (
+            action === '-' ||
+            action === '+' ||
+            action === 'x' ||
+            action === '/' ||
+            action === '%' ||
+            action === 'plusMinus'
+        ) {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+                const calcValue = calculate(firstValue, operator, secondValue)
+                displayPress.textContent = calcValue
+
+                calculator.dataset.firstValue = calcValue
+            } else {
+                calculator.dataset.firstValue = displayedNum
+            }
+
+            displayPress.textContent = displayedNum + " " + action + " "
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.operator = action
+        }
+
+        if (action === 'decimal') {
+            if (!displayedNum.includes('.')) {
+                displayPress.textContent = displayedNum + '.'
+            } else if (
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
+                displayPress.textContent = '0.'
+            }
+            calculator.dataset.previousKeyType = 'decimal'
+        }
+
+        if (action === 'clear') {
+            display.textContent = 0
+            key.textContent = 'AC'
+            calculator.dataset.previousKeyType = 'clear'
+        }
+
+        if (action === 'cancelEntry') {
+            console.log('cancel Entry!')
+        }
+
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum
+                    secondValue = calculator.dataset.modValue
+                }
+
+                display.textContent = calculate(firstValue, operator, secondValue)
+            }
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
+        }
     }
-
-        function subtracao() {
-            resultado = n1 - n2;
-            alert(`${n1} - ${n2} = ${resultado}`)
-            novaOperacao()
-    }
-
-        function multiplicacao() {
-            resultado = n1 * n2;
-            alert(`${n1} * ${n2} = ${resultado}`)
-            novaOperacao()
-    }
-   
-        function divisaoReal() {
-            resultado = n1 / n2;
-            alert(`${n1} / ${n2} = ${resultado}`)
-            novaOperacao()
-    }
-
-        function divisaoInteira() {
-            resultado = n1 % n2;
-            alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
-            novaOperacao()
-    }
-
-    function potenciacao() {
-        resultado = n1 ** n2;
-        alert(`${n1} elevado a ${n2}ª é igual a ${resultado}`)
-        novaOperacao()
-    }
-
-function novaOperacao() {
-    let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não')
-
-    if (opcao == 1) {
-        calculadora();
-    } else if (opcao == 2) {
-        alert('Até Mais!')
-    } else {
-        alert('Digite uma opção válida!')
-        novaOperacao();
-    }
-}
-
-if (operacao == 1) {
-    soma();
-} else if (operacao == 2) {
-    subtracao();
-} else if (operacao == 3) {
-    multiplicacao();
-} else if (operacao == 4) {
-    divisaoReal();
-} else if (operacao == 5) {
-    divisaoInteira();
-} else if (operacao == 6) {
-    potenciacao();
-}
-}
-}
-
-calculadora();
+})
